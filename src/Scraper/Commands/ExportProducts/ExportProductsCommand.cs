50fde41d4d3d68547c10dd9c35808bd51dbabfd6
Stafford Williams <stafford.williams@gmail.com>
2020-08-05 22:29:24 +1000

    web

--- src/Scraper/Commands/ExportProducts/ExportProductsCommand.cs
+++ src/Scraper/Commands/ExportProducts/ExportProductsCommand.cs
@@ -7,7 +7,7 @@ namespace coach_bags_selenium
     [Command("export")]
     public class ExportProductsCommand : Request
     {
-        [Option("-p|--page-size", CommandOptionType.SingleValue)]
+        [Option("-p|--page-size", CommandOptionType.SingleOrNoValue)]
         public int PageSize { get; set; } = 25;
 
         [Option("-a|--all", CommandOptionType.NoValue)]
