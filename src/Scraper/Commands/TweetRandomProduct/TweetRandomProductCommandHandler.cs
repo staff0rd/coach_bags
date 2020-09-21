ecf0c4e61b0af44f21e324ccc78c5f97b6731ecc
Stafford Williams <stafford.williams@gmail.com>
2020-09-21 10:25:42 +1000

    hashtags

--- src/Scraper/Commands/TweetRandomProduct/TweetRandomProductCommandHandler.cs
+++ src/Scraper/Commands/TweetRandomProduct/TweetRandomProductCommandHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using coach_bags_selenium.Data;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -40,7 +41,10 @@ namespace coach_bags_selenium
                 product.LastPostedUtc = request.Since;
                 var images = await _mediator.Send(new GetImagesCommand{ Category = request.Category, Product = product });
                 product.Images = images.S3Uploaded.ToArray();
-                var text = $"{product.Brand} - {product.Name} - {product.SavingsPercent}% off, was ${product.Price}, now ${product.SalePrice} {product.Link}";
+                var brandHashtag = new HashtagGenerator().Generate(product.Brand);
+                var categoryHashtag = GetTypeHashtag(product.Category);
+
+                var text = $"{product.Brand} - {product.Name} - {product.SavingsPercent}% off, was ${product.Price}, now ${product.SalePrice} {product.Link} {brandHashtag} {categoryHashtag}";
 
                 if (!request.PrepareOnly)
                 {
@@ -60,6 +64,22 @@ namespace coach_bags_selenium
                 _logger.LogWarning("Nothing new to tweet");
 
             return Unit.Value;
+        }
+
+        private string GetTypeHashtag(Category category)
+        {
+            switch (category) {
+                case Category.CoachBags: return "#bags";
+                case Category.FwrdShoes: return "#shoes";
+                case Category.FwrdDresses: return "#dresses";
+                case Category.FwrdBags: return "#bags";
+                case Category.OutnetCoats: return "#coats";
+                case Category.FarfetchDresses: return "#dresses";
+                case Category.FarfetchShoes: return "#shoes";
+                case Category.OutnetShoes: return "#shoes";
+            }
+
+            throw new ArgumentException("Unknown category");;    
         }
 
         private static IEnumerable<IMedia> UploadImagesToTwitter(IEnumerable<byte[]> images)
